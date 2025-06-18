83e2519305543a6699fb6cc01744e74d491cfabf
Stefan Oehrli <stefan.oehrli@accenture.com>
2025-06-18 11:15:07 +0200

    remove dublicate line

--- bin/update_project.sh
+++ bin/update_project.sh
@@ -48,7 +48,6 @@ fi
 TMP_DIR=$(mktemp -d)
 BACKUP_DIR="${TARGET_DIR}_backup_$(date +%Y%m%d%H%M%S)"
 
-echo "🛠️ Starting in-place update of AutoUpgrade project in: ${TARGET_DIR}"
 echo "🛠️  Starting in-place update of AutoUpgrade project in: ${TARGET_DIR}"
 
 echo "🔄 Backing up existing project (excluding *.zip, *.log, cfgtoollogs)..."
